e68b042388c3c6ae20884e2d2884268dc00e0625
Lovro Mažgon <lovro.mazgon@gmail.com>
2022-09-21 15:35:52 +0200

    add goreleaser

--- Makefile
+++ Makefile
@@ -2,8 +2,10 @@ GOLINT := golangci-lint
 
 .PHONY: build test
 
+VERSION=$(shell git describe --tags --dirty --always)
+
 build:
-	go build -o conduit-connector-google-sheets cmd/connector/main.go
+	go build -ldflags "-X 'github.com/conduitio-labs/conduit-connector-google-sheets.version=${VERSION}'" -o conduit-connector-google-sheets cmd/connector/main.go
 	go build -o google-token-gen cmd/tokengen/main.go
 
 test:
--- spec.go
+++ spec.go
@@ -20,13 +20,18 @@ import (
 	sdk "github.com/conduitio/conduit-connector-sdk"
 )
 
+// version is set during the build process (i.e. the Makefile).
+// It follows Go's convention for module version, where the version
+// starts with the letter v, followed by a semantic version.
+var version = "v0.0.0-dev"
+
 // Specification returns the Plugin's Specification.
 func Specification() sdk.Specification {
 	return sdk.Specification{
 		Name:        "google-sheets",
 		Summary:     "Google Sheets plugin",
 		Description: "A plugin capable of fetching records (in JSON format) from google spreadsheet.",
-		Version:     "v0.1.0",
+		Version:     version,
 		Author:      "Gophers Lab Technologies Pvt Ltd",
 	}
 }
